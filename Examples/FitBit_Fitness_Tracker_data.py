bdfa9213bdb63c3dfb34732c8ce91c614dc500e7
Igor Kheyfets <i.a.kheyfets@gmail.com>
2021-07-16 18:19:14 -0400

    Dataset to go along with first example

--- Examples/FitBit_Fitness_Tracker_data.py
+++ Examples/FitBit_Fitness_Tracker_data.py
@@ -2,7 +2,18 @@
 # https://www.kaggle.com/arashnic/fitbit
 # I am using dailyActivity_merged.csv file
 
+# Variables checked in this example are:
+    # Total Steps
+    # Total Distance
+    # Calories
 
-import csv
+from benford_plot import *
+import pandas
 
+fitbit_df = pandas.read_csv('Datasets/dailyActivity_merged.csv')
 
+v = ['TotalSteps', 'TotalDistance', 'Calories']
+for variable in v:
+    data = fitbit_df[variable]
+    digits = count_digits(extract_digits(data))
+    plot_distribution(digits, variable, png=True)
